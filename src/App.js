28763b273adab8af2f9f5365cff78679f4909ec6
MagicRom06 <romainhenry60@gmail.com>
2021-10-30 19:53:40 +0200

    render variable in jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 function App() {
 
-  const title = 'React';
+  const welcome = (title) => {
+    return title
+  };
 
   return (
     <div className="App">
-      <h1>Hello {title}</h1>
+      <h1>Hello {welcome('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
