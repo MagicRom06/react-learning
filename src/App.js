24f15691bec71e3af262b2ff4bdbf4c122e05a1e
MagicRom06 <romainhenry60@gmail.com>
2021-10-30 20:41:24 +0200

    first component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 import './App.css';
 
-function App() {
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
-  const list = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
+function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
-      <hr />
-      {list.map(elt => {
-        return (
-          <div key={elt.objectID}>
-            <span>
-              <a href={elt.url}>{elt.title}</a>
-            </span>
-            <span>{elt.author}</span>
-            <span>{elt.num_comments}</span>
-            <span>{elt.points}</span>
-          </div>
-        )
-      })}
+    <h1>Hello</h1>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+    
+    <hr />
+    <List />
     </div>
-  );
-}
-
-export default App;
+      );
+    }
+    
+    const List = () => {
+      return list.map(elt => (
+        <div key={elt.objectID}>
+        <span>
+        <a href={elt.url}>{elt.title}</a>
+        </span>
+        <span>{elt.author}</span>
+        <span>{elt.num_comments}</span>
+        <span>{elt.points}</span>
+        </div>
+      ))
+    }
+    
+    export default App;
+    
