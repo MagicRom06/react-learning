9565842217fdcdf1a418c84c97f7b6ebc69e84f4
MagicRom06 <romainhenry60@gmail.com>
2021-10-31 15:18:02 +0100

    callback handler

--- src/App.js
+++ src/App.js
@@ -23,28 +23,43 @@ const App = () => {
     }
   ]
 
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
+  const handleSearch = event => {
+    console.log(event.target.value);
   }
 
   return (
     <div className="App">
     <h1>Hello</h1>
-    <label htmlFor="search">Search: </label>
-    <input 
-      onChange={handleChange} 
-      id="search"
-      type="text" 
-    />
-    <p>
-      Searching for <strong>{searchTerm}</strong>
-    </p>
+
+    <Search onSearch={handleSearch} />
 
     <hr />
 
     <List list={stories} />
+    </div>
+  )
+}
+
+const Search = props => {
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  };
+  
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input 
+        onChange={handleChange} 
+        id="search"
+        type="text" 
+      />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   )
 }
