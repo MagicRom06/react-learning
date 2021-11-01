777e31d73a8b55b84ca236ded80e177733683872
MagicRom06 <romainhenry60@gmail.com>
2021-11-01 17:37:54 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -54,12 +55,12 @@ const App = () => {
   const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
     dispatchStories({type: 'STORIES_FETCH_INIT'});
-    fetch(url)
-    .then(response => response.json())
+    axios
+    .get(url)
     .then(result => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.hits,
+        payload: result.data.hits,
       })
     })
     .catch(() => 
