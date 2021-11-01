63f91dffbff6d015fd0c0488852992f2005af24d
MagicRom06 <romainhenry60@gmail.com>
2021-11-01 16:28:40 +0100

    fetch third party api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -53,10 +36,6 @@ const storiesReducer = (state, action) => {
   }
 }
 
-const getAsyncStories = () => 
-  new Promise((resolve, reject) => 
-    setTimeout(reject, 2000));
-
 const App = () => {
 
   const [searchTerm, setSearchTerm] = React.useState('');
@@ -70,10 +49,12 @@ const App = () => {
 
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    getAsyncStories().then((result) => {
+    fetch(`${API_ENDPOINT}react`)
+    .then(response => response.json())
+    .then((result) => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.hits,
       })
       setIsLoading(false)
     }).catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
